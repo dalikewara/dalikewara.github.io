67f38e96f2c8131a0e79207383f8c722a277cdfc
Dali Kewara <dalikewara@gmail.com>
2022-10-14 03:34:56 +0700

    add some plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("favicon.ico");
@@ -5,6 +8,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("google64385a25e7266f98.html");
     eleventyConfig.addPassthroughCopy("manifest.json");
     eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addPlugin(pluginMinifier);
+    eleventyConfig.addPlugin(pluginRss);
     // eleventyConfig.addPassthroughCopy("post/**/*.png");
     // eleventyConfig.addCollection("posts", function (collectionApi) {
     //     return collectionApi.getFilteredByGlob("post/**/*.md").reverse();
