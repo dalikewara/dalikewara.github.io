af1dc75cf7e132d2d66c58a83dee9e1678a9d8d5
Dali Kewara <dalikewara@gmail.com>
2022-10-21 18:10:18 +0700

    update google site verification

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("browserconfig.xml");
-    eleventyConfig.addPassthroughCopy("google64385a25e7266f98.html");
     eleventyConfig.addPassthroughCopy("manifest.json");
     eleventyConfig.addPassthroughCopy("robots.txt");
     eleventyConfig.addPassthroughCopy("post/**/*.png");
